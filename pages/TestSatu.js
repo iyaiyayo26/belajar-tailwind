505ca75f509240ed56b842fc55e26300f7eface2
iyaiyayo26 <fahiyanmr26@gmail.com>
2022-09-16 13:59:10 +0700

    belajar spacing, sizing, typography, colors

---
+++ pages/TestSatu.js
@@ -0,0 +1,18 @@
+export default function TestSatu() {
+  return (
+    <section className="px-10 py-5 font-inter bg-lime-200 mx-auto m-10 w-6/12">
+      <h2 className="text-center text-6xl font-bold mb-8 text-transparent bg-gradient-to-r from-blue-600 to-cyan-300 bg-clip-text">Belajar Mengenai Typography pada TailwindCSS</h2>
+      <div className="text-gray-600 mb-8 ml-5">
+        By <span className="text-blue-600">Lily</span>, 15 Agustus 2022
+      </div>
+      <p className="mb-4">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Sed vel excepturi, et mollitia provident incidunt corporis magnam officiis at dignissimos facere earum delectus tenetur, dolores sit vero cum, dolorem quo odit commodi
+        necessitatibus consequuntur pariatur. Suscipit esse quae placeat corrupti, ipsam quos sint atque libero tenetur itaque doloribus doloremque? Consequatur!
+      </p>
+      <blockquote className="mb-4 text-gray-400 italic">
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Aperiam aspernatur unde enim velit et placeat porro rerum repellat pariatur, sunt ab ea! Ipsa, impedit ullam nostrum id cumque expedita nam?"
+      </blockquote>
+      <p className="mb-4">Lorem ipsum dolor sit amet consectetur adipisicing elit. Nam sequi modi quo voluptas, facilis deleniti illum incidunt possimus? Reprehenderit, vero.</p>
+    </section>
+  );
+}
